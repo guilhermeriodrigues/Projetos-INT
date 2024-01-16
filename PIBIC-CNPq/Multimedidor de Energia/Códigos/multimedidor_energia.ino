0475c5c77ed4dba00679de74b19fb27861e6d7d6
Guilherme Rodrigues <100430921+guilhermeriodrigues@users.noreply.github.com>
2024-01-16 20:46:55 -0300

    Atualização do código
    O código foi atualizado para a versão de prototipagem e teste do ScadaBR com Arduino Mega, Shield Ethernet, 1 sensor tipo sonda de temperatura, 1 sensor de tensão e 1 sensor de corrente elétrica.

--- PIBIC-CNPq/Multimedidor de Energia/Códigos/multimedidor_energia.ino
+++ PIBIC-CNPq/Multimedidor de Energia/Códigos/multimedidor_energia.ino
@@ -2,6 +2,9 @@
 #include <OneWire.h> //Biblioteca do sensor de temperatura tipo sonda DS18B20
 #include <DallasTemperature.h> //Biblioteca do sensor de temperatura tipo sonda DS18B20
 #include <SPI.h> //Biblioteca ref. ao sensor de corrente elétrica SCT-013-030
+#include <Ethernet.h>
+#include <Modbus.h>
+#include <ModbusIP.h>
 
 #define CAL1 211.6 //Este valor deve ser ajustado com o auxílio de um multímetro
 #define CAL2 60 //Este valor deve ser ajustado com o auxílio de um outro instrumento
@@ -9,33 +12,60 @@
 #define PIN_TENS A6 //Define o pino do sensor de tensão
 #define PIN_CORR A7 //Define o pino do sensor de corrente
 
-EnergyMonitor ddp; //Uma instância do sensor de tensão é criada
-EnergyMonitor ca; //Uma instância do sensor de corrente é criada
+EnergyMonitor teste; //Uma instância do sensor de tensão e corrente é criada
 OneWire onewire(PIN_SONDA); //Uma instância ref. a sonda passando o pino como parâmetro
 DallasTemperature sonda(&onewire); // Passa o endereço de onewire
 
-int rede = 220; //Declaração da tensão da rede elétrica
+const int sensor1 = 0; //Offset do sensor de tensão
+const int sensor2 = 1; //Offset do sensor de corrente
+const int sensor3 = 2; //Offset do sensor de temperatura
+const int aux1 = 3; //Offset da potência ativa
+const int aux2 = 4; //Offset da potência aparente
+const int aux3 = 5; //Offset do fator de potência
+
+ModbusIP mb; //Instância do objeto Modbus
 
 void setup(){  
 
+  byte mac[] = {0x01, 0x23, 0xBE, 0xFE, 0xAF, 0xA5}; //Definição do endereço MAC
+  byte ip[] = {192,168,1,173} //Definição do endereço IP
+  mb.config(mac,ip); //Configurando o Modbus IP
+
+  //Atribuição de Input Register
+  mb.addIreg(sensor1);
+  mb.addIreg(sensor2);
+  
+  //Atribuição de Input Status
+  mb.addIsts(sensor3);
+  
   serial.begin(9600); //Define-se a taxa de 9600 bits por segundo
-  ddp.voltage(PIN_TENS, CAL1, 1.7); //São os parâmetros: (PINO ANALÓGIO, VALOR DE CALIBRAÇÃO, MUDANÇA DE FASE)
-  ca.current(PIN_CORR, CAL2); //São os parâmetros: (PINO ANALÓGIO, VALOR DE CALIBRAÇÃO)
+  teste.voltage(PIN_TENS, CAL1, 1.7); //São os parâmetros: (PINO ANALÓGIO, VALOR DE CALIBRAÇÃO, MUDANÇA DE FASE)
+  teste.current(PIN_CORR, CAL2); //São os parâmetros: (PINO ANALÓGIO, VALOR DE CALIBRAÇÃO)
   sonda.begin(); //Inicializa a sonda ao ligar o Arduino
 }
 
 void loop(){
+
+  mb.task(); //Loop de atualização do ModbusIP
   
-  ddp.calcVI(17,2000); //São os parâmetros: (17 SEMICICLOS, TEMPO LIMITE PARA FAZER A MEDIÇÃO)  - 17 semiciclos está relacionado a 60Hz
-  float tensaorms   = ddp.Vrms; //Recebe a tensão eficaz
-
+  teste.calcVI(17,2000); //São os parâmetros: (17 SEMICICLOS, TEMPO LIMITE PARA FAZER A MEDIÇÃO)  - 17 semiciclos está relacionado a 60Hz
   sonda.requestTemperatures(); //Solicita o valor da temperatura
+  float tensaorms   = teste.Vrms; //Recebe a tensão eficaz
   float temperatura = sonda.getTempCByIndex(0)); //Recebe o valor da temperatura
+  float correnterms = teste.Irms //Recebe o valor da corrente eficaz
+  float potativa = teste.realPower;
+  float potaparente = teste.apparentPower;
+  float fp = teste.powerFactor;
+  
 
-  float correnterms = ca.calcIrms(1480); //Recebe o valor da corrente eficaz
+  //Envio para o ScadaBR
 
-  //Print no terminal
-
-  serial.print("Tensão:"+ String(tensaorms)+" v, corrente: "+String(correnterms)+" A, temperatura:"+String(temperatura)+" °C);
+  mb.Ireg(sensor1, tensaorms);
+  mb.Ireg(sensor2, correnterms);
+  mb.Ists(sensor3, temperatura);
+  mb.Ireg(aux1, potativa);
+  mb.Ireg(aux2, potaparente);
+  mb.Ireg(aux3, fp);
+  
   delay(1000); //Atualização de 1 segundo
 }
